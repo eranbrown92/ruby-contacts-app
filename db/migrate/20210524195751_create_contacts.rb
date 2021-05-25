9cf781dd5acd6c99d4720fb6d6ae3d8fb826d2a3
Eran Brown <eranbrown92@gmail.com>
2021-05-25 19:42:14 +0000

    hello

---
+++ db/migrate/20210524195751_create_contacts.rb
@@ -0,0 +1,11 @@
+class CreateContacts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :contacts do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :rank
+
+      t.timestamps
+    end
+  end
+end
